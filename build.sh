7e96383d8c25f643683234e388779e4f9cff1edb
Nick Roth <nlr06886@gmail.com>
2015-06-26 00:35:09 -0500

    Initial building version using alpine linux

--- build.sh
+++ build.sh
@@ -4,9 +4,9 @@ set -x;
 set -e;
 mkdir -p wheelhouse;
 
-docker build -t deployme-base -f base.docker .;
-docker build -t deployme-builder -f build.docker .;
+docker build -t docker-python-deploy-base -f base.docker .;
+docker build -t docker-python-deploy-builder -f build.docker .;
 docker run --rm \
        -v "$(pwd)":/application -v "$(pwd)"/wheelhouse:/wheelhouse \
-       deployme-builder;
-docker build -t deployme-run -f run.docker .;
+       docker-python-deploy-builder;
+docker build -t docker-python-deploy-run -f run.docker .;
--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-exec docker run --rm -it -p 8081:8081 deployme-run;
+exec docker run --rm -it -p 8081:8081 docker-python-deploy-run;
