84bd69dea59fbb532e0dd107828bfd7ba6f965e0
Nick Roth <nlr06886@gmail.com>
2015-07-08 08:48:32 -0500

    Python builder working, but not yet with the testing image. Primary issue still out there is python 3.4.3 fails to build.

--- build_python.sh
+++ build_python.sh
@@ -1,7 +1,11 @@
+#!/bin/bash
+
+set -x;
+set -e;
+
 rm -rf .python/*
 
 docker build -t python-builder -f python-build.docker .
 
-docker run -rm --env PYBUILD_VERSION=2.7 -v "$(pwd)"/.python:/usr/python -it python-builder
-
-docker run -rm --env PYBUILD_VERSION=3.4 -v "$(pwd)"/.python:/usr/python -it python-builder
+docker run --rm -v "$(pwd)"/.python:/root/.pyenv/versions -it python-builder install -v 2.7.10
+docker run --rm -v "$(pwd)"/.python:/root/.pyenv/versions -it python-builder install -v 3.4.3
